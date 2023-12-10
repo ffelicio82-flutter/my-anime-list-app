a262c853e4109aae595be392bf87cb8336a46657
ffelicio82 <fernando_felicio82@yahoo.com.br>
2023-12-10 12:38:20 -0300

    feat: group data feature files

--- packages/data/lib/data.dart
+++ packages/data/lib/data.dart
@@ -3,5 +3,6 @@
 /// More dartdocs go here.
 library;
 
-
+export 'package:data/src/datasource/datasource.dart';
+export 'package:data/src/repository/repository.dart';
 // TODO: Export any libraries intended for clients of this package.
